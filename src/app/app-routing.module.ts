30b1d12fd0e52b875a847a27834e5d29d9db95f5
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2020-04-06 11:59:29 -0400

    Routing Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ListsComponent } from './lists/lists.component';
 import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'lists', pathMatch: 'full' },
     { path: 'lists', component: ListsComponent },
     { path: 'about', component: AboutComponent }
 ];
