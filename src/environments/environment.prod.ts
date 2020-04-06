c400ca921ce86febab159a6cfdb7174f69d28b1d
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2020-04-06 00:12:28 -0400

    First commit. Added AngularFire and Material Added Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBPLI-7A1CgDoySmvfFSNbSDV8Sv37fB40",
+    authDomain: "voice-list.firebaseapp.com",
+    databaseURL: "https://voice-list.firebaseio.com",
+    projectId: "voice-list",
+    storageBucket: "voice-list.appspot.com",
+    messagingSenderId: "877832688718",
+    appId: "1:877832688718:web:f5154c472b1bf03a1d9f1f",
+    measurementId: "G-QYM471FZRC"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBPLI-7A1CgDoySmvfFSNbSDV8Sv37fB40",
+    authDomain: "voice-list.firebaseapp.com",
+    databaseURL: "https://voice-list.firebaseio.com",
+    projectId: "voice-list",
+    storageBucket: "voice-list.appspot.com",
+    messagingSenderId: "877832688718",
+    appId: "1:877832688718:web:f5154c472b1bf03a1d9f1f",
+    measurementId: "G-QYM471FZRC"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
