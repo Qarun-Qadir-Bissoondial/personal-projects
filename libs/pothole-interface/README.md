dbc71c5f238391be9d212a303a1661b31df2ee6d
Qarun-Qadir-Bissoondial <qarun80@hotmail.com>
2020-04-28 11:15:31 -0400

    Added interface for pothole

---
+++ libs/pothole-interface/README.md
@@ -0,0 +1,7 @@
+# pothole-interface
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `ng test pothole-interface` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/pothole-interface/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'pothole-interface',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/libs/pothole-interface'
+};
