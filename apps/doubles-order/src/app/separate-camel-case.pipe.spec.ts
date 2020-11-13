d23c16ce6e2697b7f2906724f2cf03647cdaf2e7
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2020-11-12 20:16:42 -0400

    removed useless projects. updated nx config

--- apps/doubles-order/src/app/separate-camel-case.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SeparateCamelCasePipe } from './separate-camel-case.pipe';
-
-describe('SeparateCamelCasePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SeparateCamelCasePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
